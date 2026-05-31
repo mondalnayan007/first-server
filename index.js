57da724a832f71776a1bf4168e2db60ad51c3f93
Nayan Mondal <n007kingkong@gmail.com>
2026-05-31 21:28:41 +0600

    getting data from the server

--- index.js
+++ index.js
@@ -2,12 +2,25 @@
 
 const express = require('express');
 const app = express();
+
+const phones = require('./phones.json')
 const port = 4000;
 
 
 app.get('/', (req,res)=>{
     res.send('Helllooooo')
 
+})
+
+app.get('/phones', (req,res)=>{
+    const id = req.params.id;
+    console.log(id);
+    res.send(phones)
+})
+app.get('/phones/:id', (req,res)=>{
+    const id = req.params.id;
+    const phone = 
+    res.send(phones)
 })
 
 
